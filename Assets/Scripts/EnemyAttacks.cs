797de36e4c7a56c5cadf9f810fdbec62e75385c9
Neil Shah <warriors18672@gmail.com>
2018-01-27 14:42:00 -0800

    Player health and enemy damage
    Player has 3 health bars. Enemies remove 1 health from players upon
    contact.

---
+++ Assets/Scripts/EnemyAttacks.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class EnemyAttacks : NetworkBehaviour {
+
+	GameObject player;	
+	private bool playerInBounds = false;
+	public float timeBetweenDamage = 1.5f;
+	private float currTime = 0;
+	private bool firstCollision = true;
+	// Use this for initialization
+	void Start()
+	{
+	}
+
+	// Update is called one per frame
+	void Update()
+	{
+		
+		if (!playerInBounds) {
+			firstCollision = true;
+			currTime = 0;
+		} else if (playerInBounds && firstCollision) {
+			dealDamage ();
+			currTime += Time.deltaTime;
+			firstCollision = false;
+		} else {
+			currTime += Time.deltaTime;
+			if (currTime > timeBetweenDamage) {
+				dealDamage ();
+				currTime = 0;
+			}
+		}
+		
+	}
+
+	private void addCharacter(GameObject player)
+	{
+		this.player = player;
+	}
+
+	void OnCollisionEnter2D(Collision2D Other)
+	{
+		if (Other.gameObject == player) {
+			playerInBounds = true;
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D Other)
+	{
+		if (Other.gameObject == player) {
+			playerInBounds = false;
+		}
+	}
+
+	public void dealDamage()
+	{
+		if (playerInBounds) {
+			print ("Take Damage");
+			player.SendMessage("TakeDamage", 1);
+		}
+	}
+
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,12 @@ using UnityEngine.Networking;
 
 public class PlayerController : NetworkBehaviour {
 
+	private GameObject damage;
+
 	// Use this for initialization
 	void Start () {
-		
+		damage = GameObject.Find ("enemy");
+		damage.SendMessage ("addCharacter", gameObject);
 	}
 	
 	// Update is called once per frame
@@ -23,6 +26,7 @@ public class PlayerController : NetworkBehaviour {
     {
         //Local player is initialized to blue color
         //GetComponent<SpriteRenderer>().color = Color.clear;
+
         GetComponent<SpriteRenderer>().color = Color.blue;
     }
 }
