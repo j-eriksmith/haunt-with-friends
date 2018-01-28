aa7fcc6c2053d57a52375bfd775e399eb84ecbeb
InvalidCS <cjdipalma@yahoo.com>
2018-01-28 13:09:27 -0800

    changed sounds and did bugfixes

--- Assets/Scripts/TestController.cs
+++ Assets/Scripts/TestController.cs
@@ -7,18 +7,24 @@ public class TestController : MonoBehaviour{
     public bool moving = false;
     private const float SPEED = 2.4f;
     private Rigidbody2D rb;
-    private bool[] dir = new bool[4];
-    private AudioClip clipWood, clipCarpet, clipCon;
+    private bool[] dir;
+    private AudioClip clipWood, clipCarpet, clipStone;
     private AudioSource audioSrc;
-    private float longStepWait;
+    private float stepWait, stopStepWait;
+    private const float WALL_HIT_STEP_DELAY = 0.05f; // How long player has to be walking for footsteps to begin
+    private float moveTime = 0f;
+    private float lastStep = 0f;
+    private bool canStep = true, canStopStep = false;
 
     // Use this for initialization
     void Start () {
         rb = this.GetComponent<Rigidbody2D>();
         audioSrc = GetComponent<AudioSource>();
         clipWood = Resources.Load<AudioClip>("Audio/wooden_footsteps");
-        clipCarpet = Resources.Load<AudioClip>("Audio/");
-        longStepWait = clipWood.length / 9;
+        clipStone = Resources.Load<AudioClip>("Audio/concrete_footsteps_loop");
+        clipCarpet = Resources.Load<AudioClip>("Audio/carpet_footsteps_loop");
+        stepWait = clipWood.length / 9;
+        stopStepWait = stepWait - 0.1f;
 	}
 
     IEnumerator wood_step_sound_guard() // Terminates a tiny amount early so that it can be called again without conflict
@@ -28,24 +34,41 @@ public class TestController : MonoBehaviour{
         while(time < clipWood.length-0.05f)
         {
             time += Time.deltaTime;
-            if(stepcount <= (int)(time/longStepWait))
+            if(stepcount <= (int)(time/stepWait))
             {
                 stepcount++;
-                print("step");
-            }
+                lastStep = 0f;
+                canStep = false;
+                canStopStep = false;
+                //print("step");
+            }                  
             yield return null;
         }
     }
-	
+
 	// Update is called once per frame
 	void Update () {
 
         // Movement code
-        dir = new bool[] { Input.GetKey(KeyCode.UpArrow), Input.GetKey(KeyCode.DownArrow), 
-            Input.GetKey(KeyCode.LeftArrow), Input.GetKey(KeyCode.RightArrow) };
-        rb.velocity = new Vector2((System.Convert.ToInt32(Input.GetKey(KeyCode.RightArrow)) - System.Convert.ToInt32(Input.GetKey(KeyCode.LeftArrow))) * SPEED, 
-            (System.Convert.ToInt32(Input.GetKey(KeyCode.UpArrow)) - System.Convert.ToInt32(Input.GetKey(KeyCode.DownArrow))) * SPEED);
-        if((dir[0] ^ dir[1]) || (dir[2] ^ dir[3]))
+        // print(rb.velocity.magnitude);
+        lastStep += Time.deltaTime;
+        if(lastStep > stepWait)
+        {
+            canStep = true;
+        }
+        if(lastStep > stopStepWait)
+        {
+            canStopStep = true;
+        }
+        if (moving)
+        {
+            moveTime += Time.deltaTime;
+        }
+        else
+        {
+            moveTime = 0f;
+        }
+        if (rb.velocity.magnitude > 0.01f)
         {
             moving = true;
         }
@@ -53,22 +76,31 @@ public class TestController : MonoBehaviour{
         {
             moving = false;
         }
+        //print(moving);
+        dir = new bool[] { Input.GetKey(KeyCode.UpArrow), Input.GetKey(KeyCode.DownArrow), 
+            Input.GetKey(KeyCode.LeftArrow), Input.GetKey(KeyCode.RightArrow) };
+        rb.velocity = new Vector2((System.Convert.ToInt32(Input.GetKey(KeyCode.RightArrow)) - System.Convert.ToInt32(Input.GetKey(KeyCode.LeftArrow))) * SPEED, 
+            (System.Convert.ToInt32(Input.GetKey(KeyCode.UpArrow)) - System.Convert.ToInt32(Input.GetKey(KeyCode.DownArrow))) * SPEED);
+        
 
         // Footsteps code
-        if(moving)
+        if(moving && moveTime >= WALL_HIT_STEP_DELAY)
         {
-            if(!audioSrc.isPlaying)
+            if (!audioSrc.isPlaying && canStep)
             {
-                print("start");
+                //print("start");
                 audioSrc.PlayOneShot(clipWood);
                 StartCoroutine("wood_step_sound_guard");
             }
         }
         else
         {
-            print("stop");
-            audioSrc.Stop();
-            StopCoroutine("wood_step_sound_guard");
+            if (canStopStep)
+            {
+                //print("stop");
+                audioSrc.Stop();
+                StopCoroutine("wood_step_sound_guard");
+            }
         }
 	}
 }
