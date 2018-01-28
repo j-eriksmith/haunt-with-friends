41534db3e114f8cbb70a490ffb14cd92ebf2e86f
Neil Shah <warriors18672@gmail.com>
2018-01-28 02:51:38 -0800

    sprites and some of ghost

---
+++ Assets/Scripts/Ghost.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Ghost : NetworkBehaviour {
+
+	private bool playerInBounds = false;
+	private bool alreadySpoken = false;
+	AudioSource audioSource;
+
+	void Start()
+	{
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player") {
+			playerInBounds = true;
+		}
+	}
+
+	void Update ()
+	{
+		if (playerInBounds) {
+			audioSource.Play ();
+		}
+	}
+}
